d6828d60bd5828e1ca8c13849acccd8dd3257dc4
clecius <clecius.martinkoski@gmail.com>
2022-08-26 12:07:59 -0300

    adicionados testes para novas funcionalidades

--- geocode/src/test/java/global/maplink/geocode/sync/GeocodeSyncApiTest.java
+++ geocode/src/test/java/global/maplink/geocode/sync/GeocodeSyncApiTest.java
@@ -3,6 +3,7 @@ package global.maplink.geocode.sync;
 import global.maplink.MapLinkSDK;
 import global.maplink.credentials.MapLinkCredentials;
 import global.maplink.geocode.async.GeocodeAsyncAPI;
+import global.maplink.geocode.schema.crossCities.CrossCitiesRequest;
 import global.maplink.geocode.schema.geocode.GeocodeRequest;
 import global.maplink.geocode.schema.reverse.ReverseRequest;
 import global.maplink.geocode.schema.suggestions.SuggestionsRequest;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import static global.maplink.geocode.common.Defaults.DEFAULT_CLIENT_ID;
 import static global.maplink.geocode.common.Defaults.DEFAULT_SECRET;
 import static global.maplink.geocode.schema.Type.ZIPCODE;
+import static global.maplink.geocode.schema.crossCities.CrossCitiesRequest.point;
 import static global.maplink.geocode.schema.reverse.ReverseRequest.entry;
 import static java.math.BigDecimal.ZERO;
 import static java.util.Arrays.asList;
@@ -78,6 +80,7 @@ public class GeocodeSyncApiTest {
     public void mustDelegateAllReverseToAsync() {
         val async = mock(GeocodeAsyncAPI.class);
         when(async.reverse(any(ReverseRequest.Entry[].class))).thenCallRealMethod();
+        when(async.reverse(anyList())).thenCallRealMethod();
         when(async.reverse(any(ReverseRequest.class))).thenReturn(completedFuture(new SuggestionsResult()));
         val sync = new GeocodeSyncApiImpl(async);
         val result1 = sync.reverse(entry(1, 1), entry("teste", ZERO, ZERO));
@@ -88,5 +91,22 @@ public class GeocodeSyncApiTest {
         );
         assertThat(asList(result1, result2)).doesNotContainNull();
         verify(async, times(2)).reverse(any(ReverseRequest.class));
+    }
+
+    @Test
+    public void mustDelegateAllCrossCitiesToAsync() {
+        val async = mock(GeocodeAsyncAPI.class);
+        when(async.crossCities(any(CrossCitiesRequest.Point[].class))).thenCallRealMethod();
+        when(async.crossCities(anyList())).thenCallRealMethod();
+        when(async.crossCities(any(CrossCitiesRequest.class))).thenReturn(completedFuture(new SuggestionsResult()));
+        val sync = new GeocodeSyncApiImpl(async);
+        val result1 = sync.crossCities(point(1, 1), point(ZERO, ZERO));
+        val result2 = sync.crossCities(CrossCitiesRequest.builder()
+                .point(point(1, 1))
+                .point(point(ZERO, ZERO))
+                .build()
+        );
+        assertThat(asList(result1, result2)).doesNotContainNull();
+        verify(async, times(2)).crossCities(any(CrossCitiesRequest.class));
     }
 }
